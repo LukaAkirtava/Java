745d466d4d7314090710f8ac27f443f0b9a4f891
Luka <146754982+LukaAkirtava@users.noreply.github.com>
2024-10-23 16:15:01 +0400

    initial commit

--- src/MultiDimensionalArrays.java
+++ src/MultiDimensionalArrays.java
@@ -40,7 +40,7 @@ public class MultiDimensionalArrays {
             System.out.println("enter columns:");
             int column1 = scanner.nextInt();
 
-            int[][] arr = new int[row1][column1];
+            int[][] array = new int[row1][column1];
             System.out.println("enter increment number: ");
             int k = scanner.nextInt();
 
@@ -48,13 +48,13 @@ public class MultiDimensionalArrays {
                 for (int j = 0; j < column1; j++) {
                     System.out.println("enter number " + "[" + i + "]" + "[" + j + "]" + " :");
                     int temp = scanner.nextInt();
-                    arr[i][j] = temp + k;
+                    array[i][j] = temp + k;
                 }
             }
             //printer
             for (int i = 0; i < row1; i++) {
                 for (int j = 0; j < column1; j++) {
-                    System.out.print(arr[i][j] + " ");
+                    System.out.print(array[i][j] + " ");
                 }
                 System.out.print("\n");
             }
